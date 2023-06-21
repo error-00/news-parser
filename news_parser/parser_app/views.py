ee4c01c8f312d0f849646e317ad259bee1d421a4
Kateryna <ignatevaekaterina3@gmail.com>
2023-06-21 17:19:01 +0300

    created index.html with header and settings header

--- news_parser/parser_app/views.py
+++ news_parser/parser_app/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'parser_app/index.html')
+    context = {
+        'title': 'Main',
+    }
+    return render(request, 'parser_app/index.html', context)
+
+
+def account(request):
+    context = {
+        'title': 'Account',
+    }
+    return render(request, 'parser_app/account.html', context)
